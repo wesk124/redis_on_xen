70d4ee016519e7c5cafe90d9c912fb7ea8c12e14
root <lbc0430@gmail.com>
2016-10-07 05:48:20 -0400

    happy start

---
+++ README.md
@@ -0,0 +1,29 @@
+# Project Name
+
+Redis on Xen is to run redis server on several 
+Xen guests and benchmark these server on dom0.
+
+
+## Installation
+
+The user should prepare Xen, delpoy Xen vm(s), and 
+download redis in dom0 first.
+
+
+## Usage
+
+TODO: Write usage instructions
+
+
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+TODO: Write license
---
+++ prepare.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+# copy redis files to each vm.
+# in this file, we are going to use 4 vms.
+echo "===> Copying redis files to each vm..."
+
+
+
+
+echo "===> Preparing environment for benchmarking..."
+#ssh a bunch of files into vm.
+
+
+echo "===> Start running servers in vm..."
+
+
+
+
+
+
+#  TODO:Figure out the ip congigure...
+#./redis-3.2.3/src/redis-benchmark --csv -h 10.0.0.5 > temp1.dat &
+# ./redis-3.2.3/src/redis-benchmark --csv -h 10.0.0.6 > temp2.dat &
+# ./redis-3.2.3/src/redis-benchmark --csv -h 10.0.0.7 > temp3.dat &
+# ./redis-3.2.3/src/redis-benchmark --csv -h 10.0.0.8 > temp4.dat &
+# wait
+
+
