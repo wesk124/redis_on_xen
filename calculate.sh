f96fe47a1092237fa7687646fdcb6cdf8787f8ca
Sheng Wei <capwone@gmail.com>
2016-10-19 15:26:45 -0400

    clear data & optimize the experiemnt routine.

--- calculate.sh
+++ calculate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+# set -euo pipefail
 
 # results data processing and  calculate the average and standard
 # deviation.
@@ -8,25 +8,28 @@ set -euo pipefail
 
 POSTFIX=".dat"
 TEMP="temp"
+CURRENT_PATH=`pwd`
 
-echo -n "" > set_temp.dat;
-echo -n "" > get_temp.dat;
-echo -n "" > result.csv;
+#echo -n "" > set_temp$1$POSTFIX;
+#echo -n "" > get_temp$1$POSTFIX;
+echo -n "" > result$1".csv";
+
 # First need to process data file
-for i in `seq 0 3`; do
-	cat vm$i.csv | grep "SET" | cut -d "," -f 2 | cut -d "\"" -f 2 >> "set_temp.dat"
+for i in `seq 0 $1`; do
+	cat $CURRENT_PATH/"data"$1/vm$i.csv | grep "SET" | cut -d "," -f 2 | cut -d "\"" -f 2 >> $CURRENT_PATH/data$1/"set_temp$1$POSTFIX";
 done
 
 
-for j  in `seq 0 3`; do
-	cat vm$j.csv | grep "GET" | cut -d "," -f 2 | cut -d "\"" -f 2 >> "get_temp.dat"
+for j  in `seq 0 $1`; do
+	cat $CURRENT_PATH/"data"$1/vm$j.csv | grep "GET" | cut -d "," -f 2 | cut -d "\"" -f 2 >> $CURRENT_PATH/data$1/"get_temp$1$POSTFIX"
 done
 
 
 
-cat set_temp.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq / NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "set"  ", "sum / NR ", " sqrt(sqd)}' >> result.csv
+cat $CURRENT_PATH/data$1/set_temp$1$POSTFIX | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq / NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "set"  ", "sum / NR ", " sqrt(sqd)}' >> "result"$1".csv"
 
 
-cat get_temp.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq / NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "get"  ", "sum / NR ", " sqrt(sqd)}' >> result.csv 
+cat $CURRENT_PATH/data$1/get_temp$1$POSTFIX | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq / NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "get"  ", "sum / NR ", " sqrt(sqd)}' >> "result"$1".csv"
 
-echo "please check the result in result.csv"
+
+echo "please check the result in result$1.csv"
--- launch.sh
+++ launch.sh
@@ -10,17 +10,29 @@ IP_START=$BASE_IP_NUM;
 function run_benchmark {
 	printf "please enter how many server you want for this run:\n";
 	read num_of_servers;
+	if [ $num_of_servers -gt $NUM_OF_VMS ]; then
+		echo "number of servers cannot be greater than number of vm."
+		exit
+	fi
 	printf "Running $num_of_servers server(s) benchmark will start in 3 secs...\n"
 	sleep 3;
-	mkdir $num_of_servers
+	mkdir "data"$((num_of_servers-1))
 	for i in `seq 0 $((num_of_servers-1))`; do
-		./redis-3.2.3/src/redis-benchmark -t set,get --csv -h $IP_BASE$((IP_START+$i)) > `pwd`'/'$num_of_servers'/'$PREFIX"$i".csv &
+		./redis-3.2.3/src/redis-benchmark -t set,get --csv -h $IP_BASE$((IP_START+$i)) > `pwd`'/'data$((num_of_servers-1))'/'$PREFIX"$i".csv &
 	done
 	wait
 	printf "this set of experiment finish...\n"
-	
+	printf "calculate result...\n"
+	./calculate.sh $((num_of_servers-1))
 }
 
+
+
+echo "start experiment..."
+rm -r data*
+rm result*.csv
+wait
+echo "clean old data..."
 
 while true ;
 do
